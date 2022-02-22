441e23e2ab244e13c26cbc3483d7e18d59d1ffad
jafurman <jafbird52@gmail.com>
2022-02-22 12:32:42 -0800

    Comment test

--- IdeaProjects/Project1-CS2400/src/ResizeableArrayBag.java
+++ IdeaProjects/Project1-CS2400/src/ResizeableArrayBag.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class ResizeableArrayBag<T> implements BagInterface<T> {
     private T[] bag;
     private int numberOfEntries;
-    private boolean integrityOK = false; //used in the checkInitialization class
+    private boolean integrityOK = false;
     private static final int DEFAULT_CAPACITY = 25; //initialized capacity of bag
-    private static final int MAX_CAPACITY = 10000;
+    private static final int MAX_CAPACITY = 1000000;
     //creating an empty bag whose initial capacity is 25
     public ResizeableArrayBag() {
         this(DEFAULT_CAPACITY);
@@ -16,7 +16,7 @@ public class ResizeableArrayBag<T> implements BagInterface<T> {
             @SuppressWarnings("unchecked")
             T[] tempBag = (T[]) new Object[initialCapacity]; //unchecked cast
             bag = tempBag;
-            numberOfEntries = 0;
+            numberOfEntries = 0; //comment
             integrityOK = true;
         } // end if
         else {
@@ -36,19 +36,19 @@ public class ResizeableArrayBag<T> implements BagInterface<T> {
             throw new IllegalStateException("Attempt to create a bag whose " +
                     "capacity exceeds allowed " +
                     "maximum of " + MAX_CAPACITY);
-    } // end checkCapacity (makes sure the bag contents don't exceed the allowed amount)
+    } // end checkCapacity
 
     private void doubleCapacity() {
         int newLength = 2 * bag.length;
         checkCapacity(newLength);
         bag = Arrays.copyOf(bag, newLength);
-    } //end doubleCapacity (In order to add a new entry to the bag, this class will double the size of the capacity and make that it's new length)
+    } //end doubleCapacity
 
     public boolean add(T newEntry) {
-        checkInitialization(); 
+        checkInitialization();
         boolean result = true;
-        if (isArrayFull()) { 
-            doubleCapacity(); //if it's full it will double the array like I said for doubleCapacity method)
+        if (isArrayFull()) {
+            doubleCapacity();
         } //end if
         bag[numberOfEntries] = newEntry;
         numberOfEntries++;
@@ -67,51 +67,47 @@ public class ResizeableArrayBag<T> implements BagInterface<T> {
         return result;
     } // end toArray
 
-    public boolean isFull() {
-        return numberOfEntries == bag.length;
-    } //end isFull
-
     public boolean isEmpty() {
         return numberOfEntries == 0;
     } //end isEmpty
 
     public BagInterface<T> Union(BagInterface<T> bag) {
 
-        BagInterface<T> I = new ResizeableArrayBag<T>(); //makes a new bag called I
-        T[] array = this.toArray(); //creates two new arrays, array and other (below)
+        BagInterface<T> I = new ResizeableArrayBag<T>();
+        T[] array = this.toArray();
         for (T elem : array) {
             I.add(elem);
         }
-        T[] other = bag.toArray(); //the enhanced for loop goes through the array and adds the elements to the new array
+        T[] other = bag.toArray();
         for (T elem : other) {
             I.add(elem);
         }
-        return I; //then returns I with the unionized elements of T and I bag arrays (other and array)
+        return I;
     }
 
     public BagInterface<T> Intersection(BagInterface<T> bag) {
 
-        BagInterface<T> I = new ResizeableArrayBag<T>(); //creates a new resizeableArrayBag called I
+        BagInterface<T> I = new ResizeableArrayBag<T>();
 
-        T[] array = this.toArray(); //other and array are back
+        T[] array = this.toArray();
         T[] other = bag.toArray();
 
-        boolean status[] = new boolean[other.length]; //makes sure the lengths are okay
+        boolean status[] = new boolean[other.length];
 
-        for ( int i = 0; i < array.length; i++) { //loops through the contents of the first array
-            for ( int j = 0; j < other.length; j++) { //while in a nested for loop for the contents of the second array (called other^)
-                if (other[i].equals(array[i]) && (!status[j])) { 
-                    I.add(array[i]); //adds the contents of array array
+        for ( int i = 0; i < array.length; i++) {
+            for ( int j = 0; j < other.length; j++) {
+                if (other[i].equals(array[i]) && (!status[j])) {
+                    I.add(array[i]);
                     status[j] = true;
                 }
             }
         }
-        return I; //yeah, return :)
-} //end Intersection
+        return I;
+    } //end Intersection
 
     public BagInterface<T> Difference(BagInterface<T> bag){
-        return null; //this is hard 
-        }
+        return null;
+    }
     public int getCurrentSize() {
         return numberOfEntries;
     } //end getCurrentSize
@@ -125,7 +121,7 @@ public class ResizeableArrayBag<T> implements BagInterface<T> {
                 counter++;
             } //end if
         } //end for
-        return counter; //in notes, didn't really understand well
+        return counter;
     } //end getFrequencyOf
 
     public T remove() {
@@ -142,7 +138,6 @@ public class ResizeableArrayBag<T> implements BagInterface<T> {
         while (!isEmpty())
             remove();
     } //end clear
-
     private void checkInitialization() {
         if (!integrityOK) {
             throw new SecurityException("Uninitialized object used to call an arrayBag method");
